7a61ea6d1ad131eff31df751b52bd09dff3c0a29
aarshev <61513457+aarshev@users.noreply.github.com>
2022-03-14 10:13:46 +0200

    Update Player.js

--- API/models/Player.js
+++ API/models/Player.js
@@ -3,7 +3,7 @@ const {Schema, model, Types: { ObjectId }} = require('mongoose');
 const URL_PATTERN = /^http{1}s?:\/\/(.+)/;
 
 const playerSchema = new Schema({
-    playerName : { type: String, minlength: [4, 'Player Name must be at least 4 characters long']}
+    playerName : { type: ObjectId, ref: 'Post', minlength: [4, 'Player Name must be at least 4 characters long']}
     , nationality  : { type: String, minlength: [4, 'Nationality must be at least 4 characters long'] }
     , position  : { type: String, required: true, enum: ['Goalkeeper', 'Defender', 'Midfielder', 'Attacker'] }
     , playerImage  :
